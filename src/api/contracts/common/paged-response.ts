490033288db1f70154a603acf278ed26dc00b29a
اسمك <>
2026-08-07 23:47:44 +0300

    Add API response contracts

---
+++ src/api/contracts/common/paged-response.ts
@@ -0,0 +1,8 @@
+﻿import type { ApiResponse } from "./api-response";
+
+export interface PagedResponse<T> extends ApiResponse<T[]> {
+    page: number;
+    pageSize: number;
+    totalItems: number;
+    totalPages: number;
+}
